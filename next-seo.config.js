2427065bac5d47c394bd9439436003b7107af9ef
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2023-11-14 13:24:57 +0900

    add : SEO

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+  title: "kosuke-mr4.github.io",
+  description: "kosuke murakami's personal website",
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: "https://kosuke-mr4.github.io/",
+    site_name: "kosuke-mr4.github.io",
+    images: [
+      {
+        url: "https://kosuke-mr4.github.io/genbaneko.jpeg",
+        width: 600,
+        height: 600,
+        alt: "ogp image",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary",
+  },
+};
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,11 +1,12 @@
 import fs from "fs";
 import matter from "gray-matter";
 import { marked, Renderer } from "marked";
+import { NextSeo } from "next-seo";
 
 export async function getStaticProps({ params }: any) {
   const file = fs.readFileSync(`posts/${params.slug}.md`, "utf-8");
   const { data, content } = matter(file);
-  return { props: { frontMatter: data, content } };
+  return { props: { frontMatter: data, content, slug: params.slug } };
 }
 
 export async function getStaticPaths() {
@@ -27,26 +28,38 @@ renderer.image = (href: string, title: string, text: string) => {
   return `<img src="${href}" alt="${text}" title="${title}" class="mx-auto"/>`;
 };
 
-const Post = ({ frontMatter, content }: any) => {
+const Post = ({ frontMatter, content, slug }: any) => {
   return (
-    <div className="prose prose-lg max-w-none px-8 sm:px-6 md:px-8">
-      <div className="border">
-        <img
-          className="w-full"
-          src={`/${frontMatter.image}`}
-          // width={1200}
-          // height={600}
-          alt={frontMatter.title}
-        />
+    <>
+      <NextSeo
+        title={frontMatter.title}
+        description={frontMatter.description}
+        openGraph={{
+          type: "website",
+          url: `https://kosuke-mr4.github.io/blog/${slug}`,
+          title: frontMatter.title,
+          description: frontMatter.description,
+        }}
+      />
+      <div className="prose prose-lg max-w-none px-8 sm:px-6 md:px-8">
+        <div className="border">
+          <img
+            className="w-full"
+            src={`/${frontMatter.image}`}
+            // width={1200}
+            // height={600}
+            alt={frontMatter.title}
+          />
+        </div>
+        <h1 className="mt-12 text-4xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-5xl">
+          {frontMatter.title}
+        </h1>
+        <span>{frontMatter.date}</span>
+        <div
+          dangerouslySetInnerHTML={{ __html: marked(content, { renderer }) }}
+        ></div>
       </div>
-      <h1 className="mt-12 text-4xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-5xl">
-        {frontMatter.title}
-      </h1>
-      <span>{frontMatter.date}</span>
-      <div
-        dangerouslySetInnerHTML={{ __html: marked(content, { renderer }) }}
-      ></div>
-    </div>
+    </>
   );
 };
 
