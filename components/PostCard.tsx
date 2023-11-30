987aa49206770439664d081255d354e0f4e180cf
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2023-11-30 23:59:53 +0900

    add : post

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -5,11 +5,11 @@ const PostCard = ({ post }: any) => {
     <Link href={`/blog/${post.slug}`}>
       <div className="border rounded-lg md:block flex">
         <img
-          className="md:w-full w-1/2 rounded-lg" // あ〜デフォで小さいやつ書いてオプションででかい方を指定するのね〜
+          className="md:w-full w-1/2 rounded-lg aspect-video" // あ〜デフォで小さいやつ書いてオプションででかい方を指定するのね〜
           src={`/${post.frontMatter.image}`}
           alt={post.frontMatter.title}
         />
-        <div className="px-2 py-4">
+        <div className="md:h-28 px-2 py-4">
           <h1 className="font-bold text-lg">{post.frontMatter.title}</h1>
           <span>{post.frontMatter.date}</span>
         </div>
